4bf801a8782ee396d630b789f72156f423d87ec1
Jason van den Heuvel <jason01vdh@gmail.com>
2018-10-14 07:47:50 +0200

    First Push of working project

---
+++ Assets/Teleporter/Scripts/Gun.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float shootSpeed = 100f;
+    public float impactForce = 30f;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public GameObject fpsCam;
+
+    private bool isReloading = false;
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 4f;
+
+    public GameObject reloadText;
+    public Text ammoText;
+
+    public Animation anim;
+
+    //public Camera mainCamera;
+    //public Camera aimCamera;
+
+    //private bool firstClick;
+
+    void Start()
+    {
+        OVRGazePointer.instance.RequestShow();
+
+        //firstClick = false;
+
+        //mainCamera.tag = "MainCamera";
+
+        currentAmmo = maxAmmo;
+
+        ammoText.text = "Ammo: " + currentAmmo;
+        
+        reloadText.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (OVRInput.GetDown(OVRInput.Button.Back))
+        //{ // The back button is pressed.
+        //    if (!firstClick)
+        //    { // The first click is detected, so toggle aim mode.
+        //        firstClick = true;
+        //        aimCamera.gameObject.SetActive(true);
+        //        mainCamera.tag = "Untagged";
+        //        aimCamera.tag = "MainCamera";
+        //    }
+        //    else
+        //    { // The second click detected, so toggle back.
+        //        firstClick = false;
+        //        aimCamera.gameObject.SetActive(false);
+        //        mainCamera.tag = "MainCamera";
+        //        aimCamera.tag = "Untagged";
+        //    }
+        //}
+        
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Shoot();
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Back))
+        {
+            Aim();
+        }
+
+        ammoText.text = "Ammo: " + currentAmmo;
+    }
+
+    IEnumerator Reload ()
+    {
+        isReloading = true;
+
+        reloadText.SetActive(true);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+
+        isReloading = false;
+
+        reloadText.SetActive(false);
+    }
+
+    void Aim()
+    {
+        anim = GetComponent<Animation>();
+        foreach (AnimationState state in anim)
+        {
+            state.speed = 0.5F;
+        }
+    }
+
+    void Shoot()
+    {
+        currentAmmo--;
+
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * shootSpeed;
+
+        // Destroy the bullet after 2 seconds
+        //Destroy(bullet, 2.0f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+                Destroy(bullet, 1.0f);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+        }
+    }
+}
---
+++ Assets/Teleporter/Scripts/SwitchCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+
+    public GameObject mainCameraRig;
+    public GameObject aimCameraRig;
+    private bool firstClick;
+
+    void Start()
+    {
+        mainCameraRig.SetActive(true);
+        aimCameraRig.SetActive(false);
+    }
+    void Update()
+    {
+        //This will toggle the enabled state of the two cameras between true and false each time
+        //if (OVRInput.Get(OVRInput.Button.Back))
+        //{
+        //    mainCamera.enabled = false;
+        //    aimCamera.enabled = true;
+        //}
+        //else
+        //{
+        //    mainCamera.enabled = true;
+        //    aimCamera.enabled = false;
+        //}
+        
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        { // The back button is pressed.
+            if (!firstClick)
+            { // The first click is detected, so toggle aim mode on.
+                firstClick = true;
+                mainCameraRig.SetActive(false);
+                aimCameraRig.SetActive(true);
+            }
+            else
+            { // The second click detected, so toggle aim mode off.
+                firstClick = false;
+                mainCameraRig.SetActive(true);
+                aimCameraRig.SetActive(false);
+            }
+        }
+    }
+}
